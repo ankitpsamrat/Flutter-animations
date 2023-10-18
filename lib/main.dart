910f5f8a56e9d2deca5ffd7dc8a27cd9bf6a6acb
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-10-18 22:35:41 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animations/widgets/animated_rotation.dart';
+import 'package:animations/widgets/animated_listview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +24,8 @@ class MyApp extends StatelessWidget {
       // home: TextStyleAnimation(),
       // home: IconAnimation(),
       // home: OpacityAnimation(),
-      home: RotationAnimation(),
+      // home: RotationAnimation(),
+      home: AnimatedListView(),
     );
   }
 }
